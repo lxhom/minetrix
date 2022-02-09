97ff6b20c9f91bfbcb3036d2f90e128655da8023
lxhom <lxhomeis@gmail.com>
2022-02-09 12:27:21 +0100

    add caching, CLI, error handling and help

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,23 @@
 <script>
+	import inspect from 'browser-util-inspect';
 	let data;
+	let newServer = ''
+	let changeServer = () => location.search = '?' + newServer
 	let server = location.search.replace('?','')
-	setInterval(async () => {
-		let res = await fetch('/api/'+server)
-		data = await res.json();
-	}, 5000)
+	let refresh = async () => {
+		if (!server) return
+		try {
+			let res = await fetch('/api/'+server)
+			data = await res.json();
+		} catch(e) { data = {error: e} }
+	}
+	refresh()
+	setInterval(refresh, 5000)
 </script>
 <div class=content>
-	{#if data && !data.error}
+	{#if server === ''}
+		Enter an URL: <input bind:value={newServer}> <button on:click={changeServer}>Go</button> or to /?server.tld to view the tablist.
+	{:else if data && !data.error}
 		{data.host} ({data.version_from_protocol}, {data.players.online}/{data.players.max}):
 		{#each data.players.sample.sort((a,b)=>a.username>b.username) as player}
 			<div class=player>
@@ -18,7 +28,7 @@
 			{+data.players.online - +data.players.sample.length} more																		 
 		{/if}
 	{:else if data && data.error}
-    Something went wrong. Check the server URL or try again later
+		Something went wrong. Check the server URL or try again later.<pre>{inspect(data.error)}</pre>
 	{:else}
 		Loading...
 	{/if}
--- index.js
+++ index.js
@@ -2,15 +2,45 @@ let express = require('express')
 let app = express()
 let mc = require('minecraft-lib')
 
+const timeout = 2000
+
+let cache = []
+let cleanCache = () => cache = cache.filter((e,i,a) => ((e.time + timeout) - (+new Date)) > 0 &&
+    a.filter(ee => ee.request === e.request).length === 1)
+let addToCache = (request, data) => cache.push({request, data, time: +new Date})
+let getFromCache = request => { cleanCache(); return cache.filter(e => e.request === request)[0]}
+let getServerInfo = async url => {
+  let data = getFromCache(url);
+  if (data) { return data.data } else {
+    let s = await mc.servers.get(url)
+    addToCache(url, s)
+    return s;
+  }
+}
+
 app.get('/api/:server', async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*")
   try {
-    let s = await mc.servers.get(req.params.server)
+    let s = await getServerInfo(req.params.server)
     res.json(s)
-  } catch(e) { res.json({error: true}) }
+  } catch(e) { res.json({error: e}) }
+})
+
+app.get('/cli/:server', async (req, res) => {
+  try {
+    let data = await getServerInfo(req.params.server)
+    let str = `${data.host} (${data.version_from_protocol}, ${data.players.online}/${data.players.max}):\n`
+    for (let player of data.players.sample.sort((a,b)=>a.username>b.username)) {
+      str += '⬡ ' + player.username + '\n'
+    }
+    res.send(str)
+  } catch(e) { res.json({error: e}) }
+})
+
+app.get('/cli/', (req, res) => {
+  res.send('Usage: curl /cli/[server.tld]')
 })
 
 app.use(express.static('frontend/public'))
 
-// module.exports = app
 app.listen(process.env.PORT)
